f279728670675fc1f083a2fefeccec84de99547e
Mario Lopez <mario.elopez@experian.com>
2014-10-09 21:49:58 -0600

    New test

--- TestDocument.java
+++ TestDocument.java
@@ -1,8 +1,9 @@
 public class TestDocument{
-
+int x;
 /*
 Just a comment
 */
+
     public static void main (String [] args){
         System.out.println("Hello World");
     }
@@ -11,3 +12,30 @@ Just a comment
 	 Another comment
 	*/
 }
+package exceptions;
+public class TestClass {
+    /*
+	Testing
+	*/
+	public static void main(String[] args) {
+        try{
+            doTest();
+        }
+        catch(MyException me){
+            System.out.println(me);
+        }
+    }
+    static void doTest() throws MyException{
+        int[] array = new int[10];
+        array[10] = 1000;
+        doAnotherTest();
+    }
+    static void doAnotherTest() throws MyException{
+        throw new MyException("Exception from doAnotherTest");
+    }
+} class MyException extends Exception {
+    public MyException(String msg){
+        super(msg);
+    }
+}
+
